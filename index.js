f256d2328eb1bfad0ce946882ff9c4b0a50c9da2
AlexeiTolstonog-ab-inbev <Alexei.Tolstonoh@ab-inbev.com>
2021-01-13 16:19:04 +0200

    create console app

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log("start")
+const readline = require('readline')
+const fs = require('fs')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Do you want to generate data ? ", function(unswer) {
+    if(unswer === 'Y') {
+        rl.question("How march data do you want to create ? ", function (count) {
+            console.log(`we need to create ${count} objects`)
+            rl.question("Input path to file? ", function (path) {
+                console.log(`You path ${path}`)
+
+                rl.question("What name of file do you want to create ? ", function (name) {
+                    console.log(`we need to create ${name} objects`)
+                    if (!(fs.existsSync(`${path}${name}`))) {
+                        fs.writeFile(`${name}`,count, (err) => {
+                            if (err) throw err;
+                            console.log(`The file has been saved2! ${count}`);
+                            rl.close();
+                        });
+                    }
+
+                        rl.question(`The path ${path}${name} exists. Do we need to rewrite this file? Y/N`, function (unswer){
+                            if(unswer === `Y`){
+                                fs.writeFile(`${name}`, count, (err) => {
+                                    if (err) throw err;
+                                    console.log(`The file has been saved1! ${count}`);
+                                    rl.close();
+                                });
+                            }
+                            console.log('ok')
+                        })
+                });
+            });
+        });
+    }
+});
+
+rl.on("close", function() {
+    console.log(`sucsessful`);
+    process.exit(0);
+});
+
