17a153ca38d6a21df1c8af2d970d421283abb496
AlexeiTolstonog-ab-inbev <Alexei.Tolstonoh@ab-inbev.com>
2021-01-15 21:21:28 +0200

    refactored code

--- src/controllers/offers.js
+++ src/controllers/offers.js
@@ -1,28 +1,28 @@
 const express = require('express')
 const offerRouter = express.Router()
-const  Offers = require('../services/offers')
+const Offers = require('../services/offers')
 
 const offerService = new Offers('file.js')
 offerService.generateData()
 
-offerRouter.get('/', (req, res)=>{
-const offers = offerService.getAllOffers()
+offerRouter.get('/', (req, res) => {
+    const offers = offerService.getAllOffers()
     res.json(offers)
 })
 
-offerRouter.get('/:id',(req, res)=>{
-const {id} = req.params
+offerRouter.get('/:id',(req, res) => {
+    const {id} = req.params
     const offer = offerService.getOffersById(id)
     res.json(offer)
 })
 
-offerRouter.post('/',(req, res)=>{
+offerRouter.post('/',(req, res) => {
    offerService.postOffer(req.body)
-res.sendStatus(200)
+    res.sendStatus(200)
 })
 
-offerRouter.delete('/:id', (req, res)=>{
-   const {id} = req.params
+offerRouter.delete('/:id', (req, res) => {
+    const {id} = req.params
     offerService.deleteOffer(id)
     res.sendStatus(200)
 })
